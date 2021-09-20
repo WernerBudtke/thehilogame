d86636e0ca0516e1f9a3f4fd1f294727c2b46b1e
WernerBudtke <68172417+WernerBudtke@users.noreply.github.com>
2021-09-20 11:33:10 -0300

    added some input limiters on register

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true, maxLength: 16},
-    password: {type: String, required: true},
+    password: {type: String, required: true, minlength: 4},
     highscore: {type: Number, default: 0},
     tries: {type: Number, default: 0},
     wins: {type: Number, default: 0},
